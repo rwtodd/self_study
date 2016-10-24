482e15c08ee8e6239c0194ed2608e23434744429
Richard Todd <rwtodd@users.noreply.github.com>
2016-10-23 21:33:29 -0500

    refactored haskell version

--- elem_num_theory/ch1_ulam/ulam.hs
+++ elem_num_theory/ch1_ulam/ulam.hs
@@ -5,27 +5,24 @@ import qualified Data.Map as M
 ulam :: Integer -> Integer -> [Integer]
 ulam a b = 
   -- we start the computation off with the given 'a', seeding the backlog map with 'b'
-  let nums = a : nextUlam a (M.singleton b 1)
+  let nums                    = a : nextUlam a (M.singleton b 1)
       nextUlam recent backlog = 
+          let -- extract the next Ulam sequence value, and the remaining backlog
+              (minKey, rest)  = getMin updated  
+                                  where -- a helper function to extract the minimum key from the map with value == 1,
+                                        -- discarding entries until a proper one is found
+                                        getMin mapping = case (M.minViewWithKey mapping) of
+                                                             Just ((x,y), r) -> if y == 1 then (x,r) else getMin r
+                                                             Nothing         -> error "ran out of keys!!!"
 
-              -- the next group of numbers to add to the backlog
-          let nextBatch             = map (+ recent) $ takeWhile (< recent) nums
+                                        -- the backlog, with new keys added from the next batch of sums
+                                        updated  = foldr mergeKeys backlog nextBatch 
+                                                      where -- a helper function to merge keys into the backlog, maxing out the
+                                                            -- value at 2... so we never overflow 
+                                                            mergeKeys key mapping = M.insertWith (\_ _ -> 2) key 1 mapping
 
-              -- a helper function to merge keys into the backlog, maxing out the
-              -- value at 2... so we never overflow 
-              mergeKeys key mapping = M.insertWith (\_ _ -> 2) key 1 mapping
-
-              -- this is teh backlog, with the extra keys added
-              updated               = foldr mergeKeys backlog nextBatch 
-
-              -- a helper function to extract the minimum key from the map with value == 1,
-              -- discarding entries until a proper one is found
-              getFirst mapping      = case (M.minViewWithKey mapping) of
-                                         Just ((x,y), r) -> if y == 1 then (x,r) else getFirst r
-                                         Nothing         -> error "ran out of keys!!!"
-
-              -- use getFirst to extract the next Ulam sequence value
-              (minKey, rest)        = getFirst updated  
+                                                            -- the next group of numbers to add to the backlog
+                                                            nextBatch = map (+ recent) $ takeWhile (< recent) nums
 
               -- the return value is the next number int he sequence, and a recursive call to
               -- continue the computation
