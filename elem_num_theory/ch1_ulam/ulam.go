941bbcdcc247f9b4800222c2ef21987708ab1fd3
Richard Todd <rwtodd@users.noreply.github.com>
2016-10-31 01:17:29 -0500

    changed representation of the ordered queue

--- elem_num_theory/ch1_ulam/ulam.go
+++ elem_num_theory/ch1_ulam/ulam.go
@@ -1,121 +1,108 @@
 package main
 
 import (
- "os"
-"fmt"
-"strconv"
- "container/heap"
+	"fmt"
+	"os"
+	"strconv"
 )
 
 type OrderedQueue interface {
-  Insert([]int64)
-  PopInt() int64
+	Insert([]int64)
+	PopInt() int64
+}
+
+// we will implement OrderedQueue as follows:
+type oqEntry struct {
+	key    int64 // the number
+	unique bool  // false == seen more than once
+}
+
+type oqArray []oqEntry
+
+func NewOrderedQueue() *oqArray {
+	return &oqArray{}
+}
+
+func (q *oqArray) PopInt() int64 {
+	idx := 0
+	for !(*q)[idx].unique {
+		idx++
+	}
+	ans := (*q)[idx].key
+	*q = (*q)[idx+1:]
+	return ans
+}
+
+// this is an n+m merge of the two arrays...
+func (q *oqArray) Insert(keys []int64) {
+	newq := make(oqArray, 0, len(keys)+len(*q))
+	idxOQ, idxKeys := 0, 0
+
+	for (idxOQ < len(*q)) && (idxKeys < len(keys)) {
+
+		comparison := (*q)[idxOQ].key - keys[idxKeys]
+		switch {
+		case comparison == 0:
+			(*q)[idxOQ].unique = false
+			newq = append(newq, (*q)[idxOQ])
+			idxOQ++
+			idxKeys++
+		case comparison < 0:
+			newq = append(newq, (*q)[idxOQ])
+			idxOQ++
+		default:
+			newq = append(newq, oqEntry{keys[idxKeys], true})
+			idxKeys++
+		}
+	}
+
+	// now copy any that were left...
+	for _, v := range (*q)[idxOQ:] {
+		newq = append(newq, v)
+	}
+	for _, v := range keys[idxKeys:] {
+		newq = append(newq, oqEntry{v, true})
+	}
+
+	// set ourself to the new queue
+	*q = newq
 }
 
 // generates an ulam sequence...
 func ulam(port chan int64, first int64, second int64) {
-   var q OrderedQueue = NewIntHeap()
-   q.Insert([]int64{first,second})
-   sofar := []int64{}
+	var q OrderedQueue = NewOrderedQueue()
+	q.Insert([]int64{first, second})
+	sofar := []int64{}
 
-   for {
-      nxt := q.PopInt()
-      port <- nxt
-      sums := make([]int64, len(sofar))
-      for idx, sfval := range sofar {
-           sums[idx] = nxt + sfval 
-      }
-      q.Insert(sums)
-      sofar = append(sofar,nxt)
-   } 
+	for {
+		nxt := q.PopInt()
+		port <- nxt
+		sums := make([]int64, len(sofar))
+		for idx, sfval := range sofar {
+			sums[idx] = nxt + sfval
+		}
+		q.Insert(sums)
+		sofar = append(sofar, nxt)
+	}
 }
-
 
 func main() {
-   if len(os.Args) < 2 {
-      fmt.Println("Need a number arg.")
-      return
-   }
-   max,_ := strconv.Atoi(os.Args[1])
-   fmt.Printf("Argument was %v\n", max)
+	if len(os.Args) < 2 {
+		fmt.Println("Need a number arg.")
+		return
+	}
+	max, _ := strconv.Atoi(os.Args[1])
+	fmt.Printf("Argument was %v\n", max)
 
-   port := make(chan int64,100) 
-   go ulam(port,1,2)
+	port := make(chan int64, 100)
+	go ulam(port, 1, 2)
 
-   count := 0
-   for x := range port {
-      count++
-      if count == max { 
-         fmt.Printf("%d:\t%d\n",count, x)
-         return
-      }
-   }
+	count := 0
+	for x := range port {
+		count++
+		if count == max {
+			fmt.Printf("%d:\t%d\n", count, x)
+			return
+		}
+	}
 }
-
-
-// the heap came from the package examples... I only 
-// hacked together a NextUnique method.
-// It would probably be much better to make an actual
-// ordered tree so I could keep a count rather than storing
-// duplicates 
-
-// An IntHeap is a min-heap of ints.
-type IntHeap []int64
-
-func NewIntHeap() *IntHeap {
-   ans := &IntHeap{}
-   heap.Init(ans)
-   return ans
-}
-
-func (h IntHeap) Len() int           { return len(h) }
-func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
-func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-
-func (h *IntHeap) Push(x interface{}) {
-// Push and Pop use pointer receivers because they modify the slice's length,
-// not just its contents.
-*h = append(*h, x.(int64))
-}
-
-func (h *IntHeap) Pop() interface{} {
-old := *h
-n := len(old)
-x := old[n-1]
-*h = old[0 : n-1]
-return x
-}
-
-
-func (h *IntHeap) Insert(vals []int64) {
-   for _,v := range vals {
-      heap.Push(h, v)
-   }
-}
-
-// Get the next item from the heap that is unique in the heap,
-// remove and return it.
-func (h *IntHeap) PopInt() int64 {
-  //step 1 get a value
-  var x  = heap.Pop(h).(int64)
-
-  for {
-     // step 2: was it the last one?  good...
-     if h.Len() == 0 { return x }
-
-     // step 3:  get another value...
-     var y = heap.Pop(h).(int64)
-
-     // step 4:  if they don't match, put second back and return first...
-     if( x != y ) {
-        heap.Push(h,y)
-        return x
-     }
-
-     // step 5: if they did match ... soak up remaining matching values...
-     for (x == y)  {
-        x = heap.Pop(h).(int64)
-     }
-   }
-}  
-
