3abf2e08d0e16b7d9adcf19a98b6baa74019cd86
Richard Todd <rwtodd@users.noreply.github.com>
2017-01-22 22:21:00 -0600

    tweaked implementation

--- elem_num_theory/ch1_ulam/ulam.lisp
+++ elem_num_theory/ch1_ulam/ulam.lisp
@@ -8,24 +8,22 @@
 
 ; Merge new candidate NUMS into the backlog ENTS
 (defun ulam-merge (nums ents)
-  (if (null nums) 
-    ents
-    (let ((result nil))
-      (do ()
-          ((null ents) (dolist (n nums) (push (ulam-ent n nil) result)) 
-                       (nreverse result))
-          (let ((a (car nums))
-                (b (ulam-num (car ents))))
-            (cond ((= a b)
-                    (push (ulam-ent a t) result)
-                    (pop nums) 
-                    (pop ents))
-                  ((< a b)
-                    (push (ulam-ent a nil) result)
-                    (pop nums))
-                  ((> a b)
-                    (push (car ents) result)
-                    (pop ents))))))))
+  (let ((result nil))
+    (do ()
+        ((null ents) (dolist (n nums) (push (ulam-ent n nil) result)) 
+                     (nreverse result))
+        (let ((a (car nums))
+              (b (ulam-num (car ents))))
+          (cond ((= a b)
+                  (push (ulam-ent a t) result)
+                  (pop nums) 
+                  (pop ents))
+                ((< a b)
+                  (push (ulam-ent a nil) result)
+                  (pop nums))
+                ((> a b)
+                  (push (car ents) result)
+                  (pop ents)))))))
 
 ; Get the first backlog number that's not a repeat, and
 ; return it CONS'ed to the rest of the backlog.  This should
@@ -41,24 +39,24 @@
         (recent (car sofar)))
     (dolist (v (cdr sofar) result) (push (+ v recent) result))))
       
+; LCONS, LCDR and LTAKE are a couple helper functions since I'm 
+; not using a lazy-list library... 
+(defmacro lcons (hd tl) `(cons ,hd (lambda () ,tl)))
+(defun lcdr (s) (funcall (cdr s)))
+(defun ltake (ll n)
+  (let ((result (list (car ll))))
+    (dotimes (idx (- n 1) (nreverse result)) 
+       (setq ll (lcdr ll))
+       (push (car ll) result))))
+
 ; Generate the next lazy-cons in the ulam stream.
 (defun ulam-next (sofar backlog)
   (let* ((batch (ulam-backl sofar))
          (merged (ulam-merge batch backlog))
          (result (ulam-first merged))
          (nval   (pop result)))
-    (cons nval (lambda () (ulam-next (cons nval sofar) result)))))
+    (lcons nval (ulam-next (cons nval sofar) result))))
 
-(defun ulam (a b) 
-  (cons a (lambda () (ulam-next (list a) (list (ulam-ent b nil))))))
-
-; LCDR and LTAKE are a couple helper functions since I'm not using
-; a lazy-list library... 
-(defun lcdr (s) (funcall (cdr s)))
-
-(defun ltake (ll n)
-  (let ((result (list (car ll))))
-    (dotimes (idx (- n 1) (nreverse result)) 
-       (setq ll (lcdr ll))
-       (push (car ll) result))))
+; Generate a lazy ulam-a,b sequence
+(defun ulam (a b) (lcons a (lcons b (ulam-next (list b a) nil))))
 
